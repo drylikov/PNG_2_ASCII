e5bb124c0482e5a21e9f4611441e8c9a495736f3
drylikov <denis.rylikov@protonmail.com>
2024-09-29 16:55:39 +0300

    PNG_2_ASCII
    Lightning fast ASCII image generator.

---
+++ README.md
@@ -0,0 +1,30 @@
+# *png2ascii*: A lightning fast PNG to ASCII image generator
+
+<img src="screenshots/title.png">
+
+*png2ascii* is an ASCII image generator written in C. It was made to be the fastest way to make ASCII versions of your favorite images.
+
+### Why?
+
+Good question. It's not like tools like this are hard to find, but it *is* hard to find one that doesn't make a mess of requiring external libraries, or one that actually performs quickly. *png2ascii* is lightweight and does only what it needs to do.
+
+### Examples
+
+<img src="screenshots/cocacola.png">
+<img src="screenshots/seahawks.png">
+
+### Features and Notes
+
+  - Super fast PNG to ASCII conversion, tuned with KCachegrind for performance.
+  - The LodePNG library is required for use and is included here. There are no external dependencies.
+  - Size and image format conversion easily done with another command line utility. See "Usage" below for details.
+  
+
+### Performance
+*png2ascii* was designed for speed, which is a large reason why it was written in C. KCachegrind was used to squeeze as much performance out of the tool as possible. *png2ascii* is fast, converting a 10MP, 10.6MB PNG into ASCII in just over 1 second on an Intel Core M processor. Compare that to *45 seconds* for a comparable conversion with a Python utility. Though there's little reason you'd ever need to produce a single image that large, *png2ascii* can be used to batch convert frames to produce cool ASCII GIFs.
+
+### Usage
+
+- Run <code>make</code> at the command line to build the project with GCC.
+- *png2ascii* expects a PNG file as its only argument and outputs the ASCII image to stdout.
+- Images with a width greater than 100 pixels or so will be very large. It's easy to resize any image to the perfect size with ImageMagick's <code>convert</code>: <code>convert img.png -resize 80x80 out.png ; ./png2ascii out.png > out.txt</code>. True <code>|</code> piping isn't yet supported, because I'm not certain that LodePNG supports reading a PNG from stdin. I'm working on it.
---
+++ png2ascii.c
@@ -0,0 +1,78 @@
+//
+//  png2ascii.c
+//  png2ascii
+//
+//  Created by Alex King on 8/15/15.
+//  Copyright © 2015 Alex King. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lodepng.h"
+
+#define NUM_ART_CHARS 10
+
+/* Converts specified PNG file and returns a pointer to converted ASCII image */
+unsigned char* png_to_ascii(const char* filename, unsigned* input_width,
+                            unsigned* input_height)
+{
+    unsigned char *image, *output;
+    unsigned width, height, error;
+    float brightness;
+    int x, index;
+    char *art_chars = "@%#*+=-:. "; /* ASCII char range, darkest to lightest */
+    
+    /* Sets image as buffer with pattern RGBARGBARGBA */
+    error = lodepng_decode32_file(&image, &width, &height, filename);
+    
+    if (error) {
+        printf("PNG decoder error %u: %s\n", error, lodepng_error_text(error));
+        printf("Error opening specified file. Exiting...\n");
+        exit(1);
+    }
+
+    output = malloc(width * height * sizeof(char));
+    *input_width = width;
+    *input_height = height;
+    
+    for (x = 0; x < (width * height * 4); x += 4) {
+        /* Compute relative luminance of RGB value */
+        brightness = (0.2126 * image[x]      +
+                      0.7152 * image[x + 1]  +
+                      0.0722 * image[x + 2]) / 255;
+        /* Set pixel in output to be nearest ASCII character to brightness. 
+           If alpha channel is transparent, set pixel as whitespace */
+        index = (image[x + 3] ? brightness * (NUM_ART_CHARS - 1) : 9);
+        output[x / 4] = art_chars[index];
+    }
+    free(image);
+    return output;
+}
+
+/* Outputs converted image as plain text to the terminal */
+void print_ascii_image(unsigned char* output, int width, int height)
+{
+    char buffer[(2 * width) + 1];
+    int x, y;
+    for (y = 0; y < (height * width); y += width) {
+        /* Only call printf once per row, cutting runtime by two thirds */
+        for (x = 0; x < width; x++) {
+            buffer[2 * x] = output[y + x];
+            buffer[(2 * x) + 1] = ' ';
+        }
+        buffer[2 * width] = '\0';
+        printf("%s\n", buffer);
+    }
+}
+
+int main(int argc, const char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: ./png2ascii filename.png\n");
+        return 1;
+    }
+    unsigned width, height;
+    unsigned char *output = png_to_ascii(argv[1], &width, &height);
+    print_ascii_image(output, width, height);
+    free(output);
+    return 0;
+}
